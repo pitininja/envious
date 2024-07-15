eb37e96e88a1bd2cd812e75183105fc6c60a53bd
Lord Grosse Jeanine <yoyojeanine@gmail.com>
2024-07-15 09:52:57 +0200

    updated dependencies

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,3 +1,3 @@
 import 'dotenv/config';
 import { type TObject, type Static } from '@sinclair/typebox';
-export declare const envious: <T extends TObject<import("@sinclair/typebox").TProperties>>(schema: T, defaultValues?: Partial<Static<T>>) => Static<T>;
+export declare const envious: <T extends TObject>(schema: T, defaultValues?: Partial<Static<T>>) => Static<T>;
